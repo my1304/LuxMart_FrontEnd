481c30fb920509a6220265573a3ea9710d3de082
Vyacheslav <my1304@t-online.de>
2025-02-21 19:48:46 +0100

    Send

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,7 @@
-export const BASE_URL = "http://localhost:8080/api";
+
+//export const BASE_URL = "http://localhost:8080/api";
 //export const BASE_URL = "https://luxmartbackend-production.up.railway.app/api";
+export const BASE_URL = "https://luxmart-backend-rv9s.onrender.com/api";
+
+//export const BASE_URL = "https://luxmartbackend-production.up.railway.app/api";
+
